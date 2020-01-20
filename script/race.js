792ebb4e0656a537b6c55d47ec31cc36df2d8ca2
FiglerReNi <renatafigler85@gmail.com>
2020-01-20 20:24:32 +0100

    forras

--- script/race.js
+++ script/race.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    $('#idTabs').idTabs();
+   getTime();
+   
+   function getTime() {
+       var date = new Date();
+       var hour = date.getHours();
+       var minute = date.getMinutes().toString();
+       var second = date.getSeconds().toString();
+       var day = '';
+       (hour < 12 ) ? day = 'AM' : day = 'PM';
+       (hour === 0) ? hour = 12 : hour;
+       (hour > 12) ? hour = hour - 12 : hour;
+       if(minute.length == 1){minute = '0' + minute}
+       if(second.length == 1){second = '0' + second}
+       $('#updateTime').html(hour + ':' + minute + ':' + second + ' ' + day)
+   }
 });
