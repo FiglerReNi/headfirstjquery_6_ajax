f120ed1b5678f553c104b471462e35106bfb5bb1
FiglerReNi <renatafigler85@gmail.com>
2020-01-28 20:12:15 +0100

    forras

--- script/race_new.js
+++ script/race_new.js
@@ -1,14 +1,37 @@
 $(document).ready(function(){
-    $.ajax({
-        url: "race.xml",
-        cache: false,
-        dataType: "xml",
-        success: function (xml) {
+    getXMLRacers();
+    startAjaxCalls();
 
-        }
-    });
+    function startAjaxCalls(){
+        setTimeout(function () {
+            getXMLRacers();
+            startAjaxCalls();
+        }, 10000)
+    }
 
-    getTime();
+    function getXMLRacers() {
+        $.ajax({
+            url: "race.xml",
+            cache: false,
+            dataType: "xml",
+            success: function (xml) {
+                $('#male_all').empty();
+                $('#female_all').empty();
+                $('#finishers_all').empty();
+                $(xml).find('runner').each(function(){
+                    var info = "<li>Name: " + $(this).find('fname').text() + " " + $(this).find('lname').text() + ". Time: " + $(this).find('time').text() + "</li>";
+                    if($(this).find('gender').text() == 'm'){
+                        $('#male_all').append(info);
+                    }
+                    else{
+                        $('#female_all').append(info);
+                    }
+                    $('#finishers_all').append(info);
+                });
+                getTime();
+            }
+        });
+    }
 
     function getTime() {
         var date = new Date();
@@ -24,3 +47,14 @@ $(document).ready(function(){
         $('#updateTime').html(hour + ':' + minute + ':' + second + ' ' + day)
     }
 });
+
+//setInterval - csak rendszeresen ismétlődő időzítésre jó, és akor is újra hívja a functiont, ha az első kör még nem is fejeződött be,
+//              de letelt az idő. Így jelen esetben ne lesz jó. Ha user input is van, vagy külső fájl nem használjuk.
+
+//self-referencing functions
+/*A self-referencing function calls itself during its normal operations. Such functions can be particularly
+useful when you need to wait for the function’s currently running operation to complete before running
+it again.
+Combine this with a setTimeout call, and you can schedule a function to run but only keep
+going if the previous call to the function was successful.
+ */
